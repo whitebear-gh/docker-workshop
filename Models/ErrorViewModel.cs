b6e34e65004486ef592521478a2d2e0ce1a1f22e
White Bear <kjawoorski@gmail.com>
2017-11-06 12:27:34 +0100

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace docker_workshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
